ac234104280c38f1aafb204a8fea682f151ec673
Anton Angeletti <angelettianton@gmail.com>
2022-08-26 10:03:02 -0500

    add a line with my major to the output

--- src/Hello.java
+++ src/Hello.java
@@ -3,6 +3,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, my name is Anton Angeletti.");
+		System.out.println("My major is Computer Science.");
 	}
 
 }
