bd1131bfaba18817ad66a2bb47712c50d375c05b
Loc Nguyen <lnguyen72@huskers.unl.edu>
2022-08-26 13:35:53 -0500

    Added my name and major to the output

--- src/Hello.java
+++ src/Hello.java
@@ -4,6 +4,8 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello, my name is Anton Angeletti.");
 		System.out.println("My major is Computer Science.");
+		System.out.println("Hello, my name is Loc Nguyen.");
+		System.out.println("My major is Computer Science.");
 	}
 
 }
