9f12e127629f359693050168f606d9554f846400
Anton Angeletti <angelettianton@gmail.com>
2022-09-16 18:07:07 -0500

    Add header documentation

--- src/unl/cse/Hello.java
+++ src/unl/cse/Hello.java
@@ -1,5 +1,12 @@
 package unl.cse;
-
+/**
+ * Anton Angeletti (aangeletti3@huskers.unl.edu)
+ * 2022/08/25
+ * 
+ * A simple "Hello, World" program that prints out my name and major
+ * @author aangelet
+ *
+ */
 public class Hello {
 
 	public static void main(String[] args) {
